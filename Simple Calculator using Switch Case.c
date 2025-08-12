7ec08cff9e98ccfa8f19576845e33150fccafe68
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-12 15:53:45 +0600

    Create Simple Calculator using Switch Case.c

---
+++ Simple Calculator using Switch Case.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main() {
+    double X, Y;
+    char operator;
+
+    // Get inputs from user
+    printf("Enter the first number: ");
+    scanf("%lf", &X);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &Y);
+
+    printf("Enter an operator (+, -, *, /, %): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            printf("Result: %.2lf + %.2lf = %.2lf\n", X, Y, X + Y);
+            break;
+
+        case '-':
+            printf("Result: %.2lf - %.2lf = %.2lf\n", X, Y, X - Y);
+            break;
+
+        case '*':
+            printf("Result: %.2lf * %.2lf = %.2lf\n", X, Y, X * Y);
+            break;
+
+        case '/':
+            if (Y == 0) {
+                printf("Error: Division by zero is not allowed.\n");
+            } else {
+                printf("Result: %.2lf / %.2lf = %.2lf\n", X, Y, X / Y);
+            }
+            break;
+        
+        case '%':
+            // If you want to use modulo, cast X and Y to int
+            printf("Result: %.0lf %% %.0lf = %.0lf\n", X, Y, (int)X % (int)Y);
+            break;
+            
+        default:
+            printf("Invalid operator!\n");
+            break;
+    }
+
+    return 0;
+}
