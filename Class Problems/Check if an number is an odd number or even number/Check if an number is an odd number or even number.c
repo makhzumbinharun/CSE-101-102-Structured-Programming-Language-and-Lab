a4d17e4866810943c15dba574d036c8098a5c5fb
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-13 17:28:44 +0600

    Added zero message

--- Class Problems/Check if an number is an odd number or even number/Check if an number is an odd number or even number.c
+++ Class Problems/Check if an number is an odd number or even number/Check if an number is an odd number or even number.c
@@ -5,7 +5,10 @@ int main() {
     int N;
     printf("Enter a Number:");
     scanf("%d", &N);
-    if (N%2==0){
+    if(N==0){
+        printf("%d is Zero", N);
+    }
+    else if (N%2==0){
         printf("%d is an Even Number.", N);
     }
     else{
