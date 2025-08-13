959fe19fee94c1d05cace32fe4f6e3d0553d22ce
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-13 18:01:35 +0600

    Create using switch-case.c

---
+++ Class Problems/Check if an number is an odd number or even number/Using Switch-Case/using switch-case.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Enter the Number: ");
+    scanf("%d", &N);
+
+    switch (N%2==0) {
+        case 1:
+            printf("Even");
+            break;
+        case 0:
+            printf("Odd");
+            break;
+    }
+
+    return 0;
+}
