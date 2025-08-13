fb30de93884edff79aeaed1c83f6e2da697f5e25
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-13 17:20:09 +0600

    Shorten the code

--- Class Problems/Check if a year is a Leap Year/Check if a year is a Leap Year.c
+++ Class Problems/Check if a year is a Leap Year/Check if a year is a Leap Year.c
@@ -5,14 +5,11 @@ int main() {
     int Year;
     printf("Enter a year:");
     scanf("%d", &Year);
-    if (Year%400==0){
-        printf("%d is a Leap Year", Year);
-    }
-        else if(Year%4==0 && Year%100!=0){
-        printf("%d is a Leap Year", Year);
+    if (Year%400==0 || (Year%4==0 && Year%100!=0)){
+        printf("%d is a Leap Year.", Year);
     }
     else{
-        printf("%d is not a Leap Year", Year);
+        printf("%d is not a Leap Year.", Year);
     }
 
     return 0;
