d786f8d9b768e0468ede4f8f4b9251fd663a5d68
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-13 17:11:47 +0600

    Added Variable Year in printf

--- Class Problems/Check if a year is a Leap Year/Check if a year is a Leap Year.c
+++ Class Problems/Check if a year is a Leap Year/Check if a year is a Leap Year.c
@@ -6,13 +6,13 @@ int main() {
     printf("Enter a year:");
     scanf("%d", &Year);
     if (Year%400==0){
-        printf("%d is a Leap Year");
+        printf("%d is a Leap Year", Year);
     }
         else if(Year%4==0 && Year%100!=0){
-        printf("%d is a Leap Year");
+        printf("%d is a Leap Year", Year);
     }
     else{
-        printf("%d is not a Leap Year");
+        printf("%d is not a Leap Year", Year);
     }
 
     return 0;
