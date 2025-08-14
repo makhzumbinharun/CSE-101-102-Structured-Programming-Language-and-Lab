27e1a800c3f653b6a354551956c6e611c6b55b8b
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-14 16:08:43 +0600

    Update Simple Interest Calculator.c

--- Class Problems/Simple Interest Calculator/Simple Interest Calculator.c
+++ Class Problems/Simple Interest Calculator/Simple Interest Calculator.c
@@ -9,7 +9,7 @@ int main() {
     printf("Enter the value of Time: ");
     scanf("%f",&T);
     //S=(P*R*T)/100;
-    printf("Your Simple Interest is: %f",S=(P*(R/100)*T)/100);
+    printf("Your Simple Interest is: %f",S=(P*R*T)/100);
     
     return 0;
 }
