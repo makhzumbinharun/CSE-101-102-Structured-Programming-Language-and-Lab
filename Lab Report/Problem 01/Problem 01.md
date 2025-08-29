cad716e0e992afb2306b53c3e7b386292612513b
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-29 19:36:56 +0600

    Revert "Lab report"

--- Lab Report/Problem 01/Problem 01.md
+++
@@ -1,7 +0,0 @@
-# Problem 01
-
-Write a C program to show the text below:
-
-My name is Mr. Doe,<br>
-I am from USA,<br>
-My employee id: 4263.
--- Lab Report/Problem 01/Solution.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("My name is Mr. Doe,\n");
-    printf("I am from USA,\n");
-    printf("My employee id: 4263.\n");
-    return 0;
-
-}
