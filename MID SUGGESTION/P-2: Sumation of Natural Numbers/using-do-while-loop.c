33af7a3e1022082aaf5e52cc6959e94418aa1b92
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-09-08 23:54:45 +0600

    Create using-do-while-loop.c

---
+++ MID SUGGESTION/P-2: Sumation of Natural Numbers/using-do-while-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N, i=1, SUM=0;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    do{
+        SUM+=i;
+        i++;
+    }
+    while(i<=N);
+    printf("Sumation of all Natural Numbers from 1 to N: %d", SUM);
+    
+    return 0;
+}
