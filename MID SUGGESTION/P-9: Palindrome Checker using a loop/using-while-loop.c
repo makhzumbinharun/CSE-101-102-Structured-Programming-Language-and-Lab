d339075ab0f35ebdfd106e14c9ffc60e99b70b73
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-09-11 00:34:33 +0600

    Create using-while-loop.c

---
+++ MID SUGGESTION/P-9: Palindrome Checker using a loop/using-while-loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N, n, rem, rev=0;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    n=N;
+    while(N>0) {
+        rem=N%10;
+        rev=((rev*10)+rem);
+        N/=10;
+    }
+    printf("%d\n", rev);
+    
+    if(n==rev)
+        printf("%d is a Palindrome.", n);
+    else
+        printf("%d is not a Palindrome.", n);
+
+    return 0;
+}
