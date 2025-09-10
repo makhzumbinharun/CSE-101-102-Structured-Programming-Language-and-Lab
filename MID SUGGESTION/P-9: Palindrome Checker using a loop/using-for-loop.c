b838be42f3db75af559881e56070e9d330e52aab
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-09-11 00:36:00 +0600

    Create using-for-loop.c

---
+++ MID SUGGESTION/P-9: Palindrome Checker using a loop/using-for-loop.c
@@ -0,0 +1,22 @@
+//9. Write a C program to input a number from the user and check if the number is a palindrome or not using a loop.
+#include <stdio.h>
+
+int main() {
+    int N, n, rem, rev=0;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    n=N;
+    for(;N>0;) {
+        rem=N%10;
+        rev=((rev*10)+rem);
+        N/=10;
+    }
+    printf("%d\n", rev);
+    
+    if(n==rev)
+        printf("%d is a Palindrome.", n);
+    else
+        printf("%d is not a Palindrome.", n);
+
+    return 0;
+}
