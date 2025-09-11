402e7c321c58237bd965611b5ba41748efc5557e
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-09-11 18:33:27 +0600

    Update Days Converter.c

--- MID SUGGESTION/P-19: Days Converter/Days Converter.c
+++ MID SUGGESTION/P-19: Days Converter/Days Converter.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int d, y, w;
+    
+    printf("Enter the days: ");
+    scanf("%d", &d);
+    y=d/365;
+    w=(d%365)/7;
+    d=(d%365)%7;
+    
+    printf("%d years %d weeks %d days.\n", y, w, d);
+
+    return 0;
+}
