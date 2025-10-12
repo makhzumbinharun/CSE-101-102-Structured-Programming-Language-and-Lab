77a4c6dff0e3790a5b91b99c4de9de292d2e3824
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-10-13 00:12:02 +0600

    Update and rename Swap using Array.c to Swap using Array with temp.c

---
+++ Class Problems/Swap using Array/Swap using Array with temp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[2];
+
+    printf("Enter first number: ");
+    scanf("%d", &arr[0]);
+
+    printf("Enter second number: ");
+    scanf("%d", &arr[1]);
+
+    printf("\nBefore swapping:\n");
+    printf("a = %d, b = %d\n", arr[0], arr[1]);
+
+    
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
+
+    printf("\nAfter swapping:\n");
+    printf("a = %d, b = %d\n", arr[0], arr[1]);
+
+    return 0;
+}
