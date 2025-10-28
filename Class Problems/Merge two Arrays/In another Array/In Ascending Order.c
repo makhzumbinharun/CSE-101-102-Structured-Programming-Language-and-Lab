5691159f36317976aa5d3484fa9ade8222a7b337
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-10-29 03:39:32 +0600

    Merge and sort two arrays in ascending order
    Refactored code to merge two arrays and sort them in ascending order. Added user prompts for input and implemented selection sort for the merged array.

--- Class Problems/Merge two Arrays/In another Array/In Ascending Order.c
+++ Class Problems/Merge two Arrays/In another Array/In Ascending Order.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
 
 int main() {
-	int N, M, i=0,j=0;
-	scanf("%d %d", &N, &M);
-	int arr1[N]; 
-	while(i<N){
-	    scanf("%d", &arr1[i]);
-	    i++;
-	}
-	int arr2[M];
-	while(j<M){
-	    scanf("%d", &arr2[j]);
-	    j++;
-	}
-	int Marr[N+M];
-	for(int i=0; i<N+M; i++){
-	    arr1[i]=Marr[i];
-	}
-	
-	while(i<N && j<M){
-	printf("%d \n %d", arr1[i], arr2[j]);
-	i++;
-	j++;
-	}
+    int N, M;
+    printf("Enter sizes of the two arrays: ");
+    scanf("%d %d", &N, &M);
+
+    int arr1[N], arr2[M];
+    printf("Enter elements of first array: ");
+    for(int i=0; i<N; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter elements of second array: ");
+    for(int i=0; i<M; i++)
+        scanf("%d", &arr2[i]);
+        
+        
+    int Marr[N+M];
+    for(int i=0; i<N; i++)
+        Marr[i]=arr1[i];
+    for(int i=0; i<M; i++)
+        Marr[N+i]=arr2[i];
+
+    // Selection sort to sort merged array
+    for(int i=0; i<N+M-1; i++) {
+        int minIndex=i;
+        for(int j=i+1; j<N+M; j++) {
+            if(Marr[j]<Marr[minIndex])
+                minIndex=j;
+        }
+        // Swap
+        if(minIndex!=i) {
+            int temp=Marr[i];
+            Marr[i]=Marr[minIndex];
+            Marr[minIndex]=temp;
+        }
+    }
+    
+// Bubble sort
+/*
+for(int i=0; i<N+M-1; i++) {
+        for(int j=0; j<N+M-1-i; j++) {
+            if(Marr[j]>Marr[j+1]) {  // Swap if current > next
+                int temp=Marr[j];
+                Marr[j]=Marr[j+1];
+                Marr[j+1]=temp;
+            }
+        }
+    }
+
+*/
+    
+    // Print merged and sorted array
+    printf("Merged and Sorted Array: ");
+    for(int i=0; i<N+M; i++)
+        printf("%d ", Marr[i]);
+    printf("\n");
+
 return 0;
 }
-
-//Write a C program to merge two arrays in another array in ascending order.
