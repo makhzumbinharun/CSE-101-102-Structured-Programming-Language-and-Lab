054939a02fa03ade127bff86a881e5992baefe0d
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-12-02 23:41:26 +0600

    Update Solution.c

--- ASSIGNMENT/Array/P-03: Sort the Array elements in ascending order/Solution.c
+++ ASSIGNMENT/Array/P-03: Sort the Array elements in ascending order/Solution.c
@@ -20,16 +20,19 @@ int main(){
     printf("%d ", arr[i]);
   }
     printf("\n");
-
-for(int i=0; i<N-1; i++){
-     for(int j=0; j<N-i-1; j++){
-         if(arr[j]>arr[j+1]){
-             int temp=arr[j];
-             arr[j]=arr[j+1];
-             arr[j+1]=temp;
-         }
-     }
+ 
+ 
+ for(int i=0; i<N; i++){
+    for(int j=i+1; j<N; j++){
+        if(arr[i]>arr[j]){
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+    }
  }
+ 
+ 
     printf("After Sorting (Ascending): ");
     for(int i=0; i<N; i++) {
         printf("%d ", arr[i]);
