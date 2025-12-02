73a09d4dce7632d2b7039a5028c09495cdfd5b4b
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-12-02 23:25:39 +0600

    Create Solution.c

---
+++ ASSIGNMENT/Array/P-03: Sort the Array elements in ascending order/Solution.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+void scanarr(int N, int arr[N]){
+    for(int i = 0 ; i< N ; i++){
+	    scanf("%d",&arr[i]);
+	}
+}
+
+int main(){
+  int N;
+  printf("Enter the size of the Array: ");
+  scanf("%d", &N);
+  int arr[N];
+  printf("Enter the elements: ");
+  scanarr(N, arr);
+  
+  printf("Before Sorting: ");
+  for(int i=0; i<N; i++){
+    printf("%d ", arr[i]);
+  }
+    printf("\n");
+
+for(int i=0; i<N-1; i++){
+     for(int j=0; j<N-i-1; j++){
+         if(arr[j]>arr[j+1]){
+             int temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+         }
+     }
+ }
+    printf("After Sorting (Ascending): ");
+    for(int i=0; i<N; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+return 0;
+}
